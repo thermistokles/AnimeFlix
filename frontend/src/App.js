fff77946dd2def76f3cea55532b371dc293c06ba
216vignesh <31122509+216vignesh@users.noreply.github.com>
2023-04-23 18:09:49 -0400

    Completed search query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
+//import SearchBar from './SearchBar';
+import TextBlock from './TextBlock';
+import Box from './Box';
+import SmallBox from './SmallBox';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+function App(){
 
-function App() {
+  // const [searchText, setSearchText] = useState('')
+  const [profileData] = useState(null)
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const response = await axios.get('/ucf');
+  //       setProfileData(response.data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   getData();
+  // }, []);
+  
+  // const handleSearch = () => {
+  //   console.log(searchText);
+  //   axios.post(
+  //     '/search',
+  //     { searchText },
+  //     //{ headers: { 'Content-Type': 'application/json' } } // Add this line
+  //   )
+  //     .then(response => {
+  //       console.log("from flask->");
+  //       console.log(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // };
+
+  const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = () => {
+    console.log(searchText);
+    axios
+      .post("/", { searchText })
+      .then((response) => {
+        console.log("from flask->");
+        console.log(response.data);
+  
+        // Filter unique anime titles
+        const uniqueTitles = new Set();
+        const uniqueResults = response.data.filter((anime) => {
+          if (uniqueTitles.has(anime.title)) {
+            return false;
+          } else {
+            uniqueTitles.add(anime.title);
+            return true;
+          }
+        });
+  
+        setSearchResults(uniqueResults);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextBlock />
+      <div>
+        <label>
+        Anime:
+        <input type="text" placeholder="Search..." name='animeName'  value = {searchText}
+        onChange={(e)=>setSearchText(e.target.value)}
+        />
+        </label>
+        <button onClick={handleSearch}>Search</button> 
+        <div key={Date.now()} className="anime-container">
+  {searchResults.map((anime) => (
+    <div key={anime.anime_id} className="anime-card">
+      <img src={anime.img_url} alt={anime.title} className="anime-img" />
+      <div className="anime-title">
+        <a href={anime.link} target="_blank" rel="noopener noreferrer">{anime.title}</a>
+      </div>
+    </div>
+  ))}
+</div>
+      </div>
+      
+      {profileData && (
+        <div>
+          <p>Profile name: {profileData.profile_name}</p>
+          <p>About me: {profileData.about_me}</p>
+        </div>
+      )}
+
+      <Box title="User Based CF" content="This is the content for box 1.">
+        <SmallBox color="red" content="1" />
+        <SmallBox color="green" content="2" />
+        <SmallBox color="blue" content="3" />
+      </Box>
+      <Box title="Item Based CF" content="This is the content for box 2."/>
+      <Box title="Content Based" content="This is the content for box 3."/>
+      {/* Add the rest of your components here */}
     </div>
   );
 }
